abd87c8ba1eca8a53d196fc6a6fa70c7f49fb099
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2024-07-17 16:56:34 +0200

    feat: Add artificial delay to ASCII read

--- read_ascii.c
+++ read_ascii.c
@@ -18,6 +18,11 @@ void run(uint8_t *dataset, size_t ds_len) {
 
   int buf_cursor = 0;
   while (pos < ds_len) {
+    // Artificial delay.
+    reef_progress((float) pos / (float) ds_len);
+    reef_sleep(0.1);
+
+    // Read file line by line.
     buf[buf_cursor++] = dataset[pos];
     if (buf_cursor == l - 1 || dataset[pos++] == '\n') {
       buf_cursor = 0;
