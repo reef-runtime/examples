302c410e6efa497abded423f932ddf368d5a8b94
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2024-07-18 04:10:12 +0200

    fix: Less frequent sleep in read ASCII

--- read_ascii.c
+++ read_ascii.c
@@ -4,30 +4,31 @@ void greet_n_times(int n) {
   for (int i = 0; i < n; i++) {
     reef_puts("Hello World!");
     reef_sleep(0.2);
-    reef_progress((float) i / (float) n);
+    reef_progress((float)i / (float)n);
   }
 }
 
 void run(uint8_t *dataset, size_t ds_len) {
   int pos = 0;
 
-  #define l 120
+#define l 120
 
-  char * buf = malloc(l + 1);
+  char *buf = malloc(l + 1);
   memset(buf, 0x0, l);
 
   int buf_cursor = 0;
   while (pos < ds_len) {
     // Artificial delay.
-    reef_progress((float) pos / (float) ds_len);
-    reef_sleep(0.1);
+    reef_progress((float)pos / (float)ds_len);
 
     // Read file line by line.
     buf[buf_cursor++] = dataset[pos];
     if (buf_cursor == l - 1 || dataset[pos++] == '\n') {
       buf_cursor = 0;
-      if (strlen(buf) > 0)
+      if (strlen(buf) > 0) {
         reef_puts(buf);
+        reef_sleep(0.1);
+      }
       memset(buf, 0x0, l);
     }
   }
